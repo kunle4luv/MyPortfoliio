9f66cddbf2db98981c66c53729aa38166ea6e772
Kunle4luv <ajisafeayokunle@gmail.com>
2025-05-13 11:57:51 +0100

    adding of the hero

--- Ayokunle_Ajisafe/src/components/Hero.jsx
+++ Ayokunle_Ajisafe/src/components/Hero.jsx
@@ -9,49 +9,51 @@ const Hero = () => {
          id="home"
         className="pt-28 lg:pt-36"
     >
-        <div className="container lg:grid lg:grid-cols-3">
+        <div className="container items-center lg:grid lg:grid-cols-2 lg:gap-10">
+            <div>
+                <div className="flex items-center gap-3">
+                    <figure className="img-box w-9 h-9 rounded-lg">
+                        <img 
+                            src="/images/avatar-1.jpg" 
+                            width={40}
+                            height={40}
+                            alt="Henry clark portrait" 
+                            className="img-cover" 
+                        />
+                    </figure>
+
+                    <div className="flex items-center gap-1.5 text-zinc-400 text-sm tracking-wide">
+                        <span className="relative w-2 h-2 rounded-full bg-emerald-400">
+                            <span className=""></span>
+                        </span>
+
+                        Available for work
+                    </div>
+                </div>
+
+                    <h2 className="">
+                        Building Scalable Modern Websites for the Future
+                    </h2>
+
+                    <div className="">
+                        ButtonPrimary
+
+                        ButtonOutline
+                    </div>
+            </div>
 
             <div className="">
-                <figure className="">
+                    <figure className="">
                     <img 
-                        src="/images/avatar-1.jpg" 
-                        width={40}
-                        height={40}
-                        alt="Henry clark portrait" 
-                        className="img-cover" 
-                    />
-                </figure>
-                <div className="">
-                    <span className="">
-                        <span className=""></span>
-                    </span>
-
-                    Available for work
-                </div>
-
-                <h2 className="">
-                    Building Scalable Modern Websites for the Future
-                </h2>
-
-                <div className="">
-                    ButtonPrimary
-
-                    ButtonOutline
-                </div>
+                        src="/images/hero-banner.png" 
+                        alt="Henry Clark"
+                        width={656}
+                        height={800}
+                        className="w-full"
+                        /> 
+                    </figure>
             </div>
-
-
-            <div className="">
-                <figure className="">
-                   <img 
-                    src="/images/hero-banner.png" 
-                    alt="Henry Clark"
-                    width={656}
-                    height={800}
-                    className="w-full"
-                    /> 
-                </figure>
-            </div>
+             
         </div>
     </section>
   )
